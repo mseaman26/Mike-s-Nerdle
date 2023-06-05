f7a84c341c5b1be0a0edf589aea507082157658e
michael seaman <mseaman26@gmail.com>
2023-06-05 01:01:46 -0700

    share button

--- nerdle-react-app/src/components/GameOver.js
+++ nerdle-react-app/src/components/GameOver.js
@@ -3,11 +3,43 @@ import { useGuessesContext } from "../utils/guessesContext";
 
 const GameOver = () => {  
 
-    const {guesses} = useGuessesContext()
+    const {guesses, nerdleNumber, classesArray} = useGuessesContext()
     const [gameOverShown, setGameOverShown] = useState(false)
     const handleCloseButton = () => {
         setGameOverShown(false)
     }
+
+    const handleShare = () => {
+        const blackTile = '⬛'
+        const purpleTile = '⬛'
+        const greenTile = '🟩'
+        let tilesString = "Mike's Wordle #"+nerdleNumber+"\n"
+        for(let i = 0; i < classesArray.length; i++){
+            if(classesArray[i] === 'guessBox correct'){
+                tilesString += greenTile
+            }else if(classesArray[i] === 'guessBox misplaced'){
+                tilesString += purpleTile
+            }else if(classesArray[i] === 'guessBox absent'){
+                tilesString += blackTile
+            }else {
+                return tilesString
+            }
+            if((i+1) % 8 === 0){
+                tilesString = tilesString+"\n"
+            }
+        }
+        if (navigator.share) {
+            navigator.share({
+                title: "Results From Mike's Nerdle:",
+                text: tilesString,
+              })
+              .then(() => console.log('Successful share'))
+              .catch((error) => console.log('Error sharing', error));
+          } else {
+            console.log('Share not supported on this browser, do it the old way.');
+          }
+    }
+
 
     useEffect(() => {
         setTimeout(() => {
@@ -19,8 +51,9 @@ const GameOver = () => {
         {gameOverShown ? (
             <div className='gameOver'>
                 <div className="gameOverCard">
-                    <button className="closeButton" onClick={handleCloseButton}>X</button>
-                    <h3>You won in {guesses.length/8} guesses</h3>
+                    <div className="closeButton" onClick={handleCloseButton}>X</div>
+                    <h3 className="gameOverHeader">You won in {guesses.length/8} guesses</h3>
+                    <div className="shareButton" onClick={handleShare}>Share</div>
                 </div>
             </div>
         ) : <></>}
