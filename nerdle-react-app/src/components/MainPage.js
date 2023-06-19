94ee0e7ab884fcf49a9ae6079d4e45c442c08f06
michael seaman <mseaman26@gmail.com>
2023-06-18 18:04:50 -0700

    sending message to parent element, specifying height

--- nerdle-react-app/src/components/MainPage.js
+++ nerdle-react-app/src/components/MainPage.js
@@ -1,11 +1,16 @@
 
-import React from "react";
+import React, { useEffect } from "react";
 import GuessesContainer from "./GuessesContainer";
 import Keyboard from "./Keyboard";
 import { useGuessesContext } from "../utils/guessesContext";
 
 
 const MainPage = () => {
+
+useEffect(() => {
+    window.parent.postMessage({ height: document.body.scrollHeight }, '*');
+    console.log('height: ',document.body.scrollHeight )
+})
 
 const{ messageText } = useGuessesContext()
     return(
